4e3cebb00ad273140cf77cfd11c0d14ce7ec3357
Stefan Rotsch <stefan.rotsch@aoe.com>
2014-02-25 08:34:02 +0100

    [BUGFIX] Link handler must be registered as a function _reference_

--- ext_localconf.php
+++ ext_localconf.php
@@ -4,7 +4,7 @@ if (!defined ('TYPO3_MODE')) {
 }
 
 // Register linkhandler for "record"
-$GLOBALS['TYPO3_CONF_VARS']['SC_OPTIONS']['tslib/class.tslib_content.php']['typolinkLinkHandler']['record'] = 'AOE\Linkhandler\Handler';
+$GLOBALS['TYPO3_CONF_VARS']['SC_OPTIONS']['tslib/class.tslib_content.php']['typolinkLinkHandler']['record'] = '&AOE\Linkhandler\Handler';
 
 // Register hooks
 $GLOBALS['TYPO3_CONF_VARS']['SC_OPTIONS']['ext/rtehtmlarea/mod3/class.tx_rtehtmlarea_browse_links.php']['browseLinksHook'][] = 'AOE\Linkhandler\Hooks\ElementBrowser';
